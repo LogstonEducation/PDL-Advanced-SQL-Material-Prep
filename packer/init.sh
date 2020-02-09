3bf9d4392c542522c84ea8e9e750adc19417f62e
Paul Logston <paul.logston@gmail.com>
2020-02-09 02:41:02 -0500

    Fix sudo bug

--- packer/init.sh
+++ packer/init.sh
@@ -13,12 +13,12 @@ sudo apt update
 sudo apt -y install postgresql-12 postgresql-client-12
 systemctl status postgresql.service
 
-cat <<EOF > /etc/postgresql/12/main/pg_hba.conf
+sudo /bin/bash -c "cat > /etc/postgresql/12/main/pg_hba.conf" <<EOF
 local   all             all                                     trust
 host    all             all             127.0.0.1/32            trust
 EOF
 
-systemctl restart postgresql.service
+sudo systemctl restart postgresql.service
 sleep 5
 psql -U postgres -c "CREATE DATABASE airline"
 
