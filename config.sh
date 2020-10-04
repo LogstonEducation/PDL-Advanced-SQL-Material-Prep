235d5ae8639018f3324d6029ae04ec2bced518c0
Paul Logston <paul.logston@gmail.com>
2020-10-04 15:41:32 -0400

    Fix config script

--- config.sh
+++ config.sh
@@ -29,4 +29,7 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Run Docker
 
+sudo docker kill pdl || true
+sudo docker rm pdl || true
+sudo docker rmi logstoneducation/pdl-advanced-sql:latest || true
 sudo docker run -p 5432:5432 --env PGDATA=pdl --detach --name pdl -t docker.io/logstoneducation/pdl-advanced-sql
